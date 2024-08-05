9f878daa98036825b762ffc56a9a8e7079947b3a
Gabriel <gabrielcssoad@hotmail.com>
2024-08-05 16:29:18 -0300

    Creating Directory

---
+++ produto/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Produto, Variacao
+
+
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta',
+                    'get_preco_formatado', 'get_preco_promocional_formatado']
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)
